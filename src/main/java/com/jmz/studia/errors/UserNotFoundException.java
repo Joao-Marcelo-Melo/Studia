f6fb17ad0fc6f9b3fc28121c0fe942e83c64941f
Joao-Marcelo-Melo <marcelo25256464@gmail.com>
2025-05-22 12:33:47 -0300

    Feat: added RestExceptionHandler

---
+++ src/main/java/com/jmz/studia/errors/UserNotFoundException.java
@@ -0,0 +1,9 @@
+package com.jmz.studia.errors;
+
+
+public class UserNotFoundException extends RuntimeException {
+
+    public UserNotFoundException() {super("User not found");}
+
+    public UserNotFoundException(String message) {super(message);}
+}
---
+++ src/main/java/com/jmz/studia/infra/exceptions/RestExceptionHandler.java
@@ -0,0 +1,18 @@
+package com.jmz.studia.infra.exceptions;
+
+import com.jmz.studia.errors.UserNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class RestExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler({UserNotFoundException.class})
+    private ResponseEntity<RestErrorMessage> userNotFoundHandler(UserNotFoundException exception) {
+        RestErrorMessage restErrorMessage = new RestErrorMessage(HttpStatus.NOT_FOUND, exception.getMessage());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(restErrorMessage);
+    }
+}
